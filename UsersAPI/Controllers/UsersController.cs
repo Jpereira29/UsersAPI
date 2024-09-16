79d5353ef7fcf7429ed4f857bbfb26a1eafd8eda
Jpereira29 <jorgepereira29ele@gmail.com>
2024-09-16 19:02:06 -0300

    feat: contoller default /

--- UsersAPI/Controllers/UsersController.cs
+++ UsersAPI/Controllers/UsersController.cs
@@ -1,21 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UsersAPI.Context;
 using UsersAPI.Models;
 
 namespace UsersAPI.Controllers
 {
-    [Route("[controller]")]
+    [Route("")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController(AppDbContext context) : ControllerBase
     {
-        private readonly AppDbContext _context;
-
-        public UsersController(AppDbContext context)
-        {
-            _context = context;
-        }
+        private readonly AppDbContext _context = context;
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
