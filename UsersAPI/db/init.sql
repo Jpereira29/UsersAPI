a795228b8c89f10203678a368c800992bf914e56
Jpereira29 <jorgepereira29ele@gmail.com>
2024-09-18 23:46:06 -0300

    Rename Users table to Usuario and update SQL statements
    The database table name has been changed from `Users` to `Usuario`.
    The `CREATE TABLE` statement now creates a table named `Usuario` with columns `id`, `nome`, and `matricula`.
    The `INSERT INTO` statements have been updated to insert data into the `Usuario` table instead of the `Users` table.

--- UsersAPI/db/init.sql
+++ UsersAPI/db/init.sql
@@ -1,12 +1,12 @@
 ﻿CREATE DATABASE IF NOT EXISTS UsersDb;
 USE UsersDb;
 
-CREATE TABLE Users (
+CREATE TABLE Usuario (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	matricula VARCHAR(10) NOT NULL
 );
 
-INSERT INTO Users (nome, matricula) VALUES ('Francisco Airton Araújo Júnior', '2023010960');
-INSERT INTO Users (nome, matricula) VALUES ('Jorge Pereira de Oliveira', '2023011027');
-INSERT INTO Users (nome, matricula) VALUES ('Rafael Guimarães Januário', '2023010600');
+INSERT INTO Usuario (nome, matricula) VALUES ('Francisco Airton Araújo Júnior', '2023010960');
+INSERT INTO Usuario (nome, matricula) VALUES ('Jorge Pereira de Oliveira', '2023011027');
+INSERT INTO Usuario (nome, matricula) VALUES ('Rafael Guimarães Januário', '2023010600');
